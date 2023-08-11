a15f6c1584abca87fad9e6effb89d0451270cd15
Maxcyclotron2 <1075356767@qq.com>
2023-08-11 05:27:37 -0700

    anything

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -20,12 +20,11 @@ fn main() {
     println!("My array! Here it is: {:?}", my_arr);
 
     let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
-    println!("This Vec is empty, see? {:?}", my_empty_vec);
+    let my_empty_vec: Vec<i32> = Vec::new();
 
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
