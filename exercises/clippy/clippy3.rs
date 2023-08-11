127d9665c9d0e626fc25f3aaeb1d0e4f2a56d0e5
Maxcyclotron2 <1075356767@qq.com>
2023-08-11 06:01:13 -0700

    anything

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -9,21 +9,21 @@
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
-        my_option.unwrap();
+        
     }
 
     let my_arr = &[
         -1, -2, -3,
-        -4, -5, -6,
+        -4, -5, -6
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec: Vec<i32> = Vec::new();
+    let my_empty_vec = vec![1, 2, 3, 4, 5];
     println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
+    std::mem::swap(&mut value_a,  &mut value_b);
     // Let's swap these two!
-    std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
