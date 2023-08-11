4ca05114b3db12ac6ab45ec2dfcb1131c0f7a67f
Maxcyclotron2 <1075356767@qq.com>
2023-08-11 05:56:01 -0700

    anything

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -13,13 +13,13 @@ fn main() {
     }
 
     let my_arr = &[
-        -1, -2, -3
-        -4, -5, -6
+        -1, -2, -3,
+        -4, -5, -6,
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
     let my_empty_vec: Vec<i32> = Vec::new();
-    
+    println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
